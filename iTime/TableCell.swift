0d092d472c2edee2a837f6405ae947a9a05d32db
Asteel7nd <kornedj@bk.ru>
2022-10-21 16:49:26 +0300

    Initial Commit

---
+++ iTime/TableCell.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  iTime
+//
+//  Created by user on 15.10.2022.
+//
+
+import UIKit
+
+class TableCell: UITableViewCell {
+
+    @IBOutlet weak var tableLabel: UILabel!
+
+}
